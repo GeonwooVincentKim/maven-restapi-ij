46349e035e91a931e0c593225f4991ad87d2b1d7
GeonwooVincentKim <kdsnop@gmail.com>
2023-07-20 17:22:15 +0900

    [Feature] - Web Unit Test

--- src/test/java/io/gw/rest/service/logic/UserServiceLogicTest.java
+++ src/test/java/io/gw/rest/service/logic/UserServiceLogicTest.java
@@ -21,7 +21,10 @@ public class UserServiceLogicTest {
     // The Function that is called before the Unit Test is executed
     @BeforeEach
     public void startUp() {
-
+        this.kim = new User("Kim", "kim@test.com");
+        this.lee = new User("Lee", "lee@test.com");
+        this.userService.register(kim);
+        this.userService.register(lee);
     }
 
     @Test
@@ -29,17 +32,21 @@ public class UserServiceLogicTest {
         User sample = User.sample();
 //        this.userService.register(sample);
         assertThat(this.userService.register(sample)).isEqualTo(sample.getId());
-        assertThat(this.userService.findAll().size()).isEqualTo(1);
+        assertThat(this.userService.findAll().size()).isEqualTo(3);
+        this.userService.remove(sample.getId());
     }
 
     @Test
     public void find() {
-
+        assertThat(this.userService.find(lee.getId())).isNotNull();
+        assertThat(this.userService.find(lee.getId()).getEmail()).isEqualTo(lee.getEmail());
     }
 
     // The Function that is called after the Unit Test is executed
     @AfterEach
     public void cleanUp() {
         //
+        this.userService.remove(kim.getId());
+        this.userService.remove(lee.getId());
     }
 }
